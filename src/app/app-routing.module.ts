67a0871c48a75ffc20670bc952d1cf7e14cfe644
Aaron <aaronochoa376@gmail.com>
2025-01-23 08:15:49 -0600

    Enlazar variables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'inicio',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'card',
+    loadChildren: () => import('./card/card.module').then( m => m.CardPageModule)
+  },
+  {
+    path: 'avatar',
+    loadChildren: () => import('./avatar/avatar.module').then( m => m.AvatarPageModule)
+  },
+  {
+    path: 'thumbnail',
+    loadChildren: () => import('./thumbnail/thumbnail.module').then( m => m.ThumbnailPageModule)
   },
 ];
 
---
+++ src/app/thumbnail/thumbnail.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail',
+  templateUrl: './thumbnail.page.html',
+  styleUrls: ['./thumbnail.page.scss'],
+  standalone: false,
+})
+export class ThumbnailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
